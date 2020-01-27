605e7952ab40d29db3d934a6e2ebab3bdc767301
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-27 13:51:38 -0800

    Add login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const Users = require('../models/user-model');
 
 router.post('/register', (req, res) => {
-  let user = req.body;
+  const user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
   Users.add(user)
@@ -18,4 +18,26 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  Users.findBy({ username })
+    .then(user => {
+      console.log('password: ', password, 'user.password: ', user.password)
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({
+          message: `Welcome, ${user.username}.`
+        });
+      } else {
+        res.status(401).json({
+          message: 'Invalid credentials!'
+        })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: 'Failed to log in.'
+      });
+    });
+});
 module.exports = router;
--- models/user-model.js
+++ models/user-model.js
@@ -2,12 +2,20 @@ const db = require('../data/db-config');
 
 module.exports = {
   find,
+  findBy,
   findById,
   add,
 }
 
 function find() {
   return db('users').select('id', 'username');
+}
+
+function findBy(filter) {
+  return db('users')
+          .select('id', 'username', 'password')
+          .where(filter)
+          .first();
 }
 
 function findById(id) {
